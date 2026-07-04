a487e05af2877f62bae7adafd448192ba879649e
fidhapk47-source <fidhapk47@gmail.com>
2026-07-04 12:12:34 +0530

    Time: 0 ms (100%), Space: 12.4 MB (51.49%) - LeetHub

---
+++ 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeZeros(self, n):
+
+        return int(str(n).replace("0",""))
+        
+
+        """
+        :type n: int
+        :rtype: int
+        """
+        
